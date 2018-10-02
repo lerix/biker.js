58e93657df33a302e5a0efb570e48183a02f247f
Tomáš Kalina <info@tomaskalina.cz>
2018-10-02 22:11:31 +0200

    init

---
+++ index.js
@@ -0,0 +1,123 @@
+var motorbike;
+var line;
+var enemies = [];
+var myScore;
+
+function startGame() {
+    motorbike = new component(30, 30, "black", 300, 800);
+    line = new component(5, 20, "black", 200, 0);
+    line.gravity = 0.05;
+    motorbike.gravity = 0.05;
+    myScore = new component("30px", "Consolas", "black", 0, 24, "text");
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.width = 400;
+        this.canvas.height = 800;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 10);
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    this.score = 0;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.gravity = 0;
+    this.gravitySpeed = 0;
+    this.update = function () {
+        ctx = myGameArea.context;
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function () {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+        this.hitBottom();
+    }
+    this.hitBottom = function () {
+        var rockbottom = myGameArea.canvas.height - this.height;
+        if (this.y > rockbottom) {
+            this.y = rockbottom;
+            this.gravitySpeed = 0;
+        }
+    }
+    this.crashWith = function (otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    }
+}
+
+function updateGameArea() {
+    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+    for (i = 0; i < enemies.length; i += 1) {
+        if (motorbike.crashWith(enemies[i])) {
+            //return;
+        }
+    }
+    myGameArea.clear();
+    myGameArea.frameNo += 1;
+    if (myGameArea.frameNo == 1 || everyinterval(150)) {
+        //Add enemies
+        x = myGameArea.canvas.width;
+        minHeight = 20;
+        maxHeight = 200;
+        height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+        enemies.push(new component(10, height, "red", x, 0));
+        enemies.push(new component(10, x - height - gap, "green", x, height + gap));
+    }
+    for (i = 0; i < enemies.length; i += 1) {
+        enemies[i].x += -1;
+        enemies[i].y += +1;
+        enemies[i].update();
+    }
+    line.newPos();
+    line.update();
+
+    myScore.text = "SCORE: " + myGameArea.frameNo;
+    myScore.update();
+    motorbike.newPos();
+    motorbike.update();
+}
+
+function everyinterval(n) {
+    return (myGameArea.frameNo / n) % 1 == 0;
+}
+
+function accelerate(n) {
+    motorbike.gravity = n;
+}
