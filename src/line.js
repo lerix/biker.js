4d779af1b3ff2ab3695678eee33e6d5990371187
Tomáš Kalina <info@tomaskalina.cz>
2018-10-17 17:05:04 +0200

    text to class

--- src/line.js
+++ src/line.js
@@ -1,5 +1,4 @@
 export class line {
-
     move = 0
 
     constructor() {
--- src/text.js
+++ src/text.js
@@ -1,12 +1,22 @@
-export function text(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-    this.update = function (myGameArea) {
+export class text {
+    width;
+    height;
+    x;
+    y;
+    color;
+
+    constructor(width, height, color, x, y) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    update(myGameArea) {
         let ctx = myGameArea.context;
         ctx.font = this.width + " " + this.height;
-        ctx.fillStyle = color;
+        ctx.fillStyle = this.color;
         ctx.fillText(this.text, this.x, this.y);
     }
 }
